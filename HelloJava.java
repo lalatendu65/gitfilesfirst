3574536c56c880c5b98e1b60a5f79023e560cff6
lalatendu65 <72854623+lalatendu65@users.noreply.github.com>
2022-05-04 11:10:34 +0530

    Update HelloJava.java
    change it

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,5 @@
 class HelloJava{
 public static void main(String args[]){
-System.out.Println("My First Java Program for github");
+System.out.Println("My First Java Program for github to jenkin ");
 }
 }
